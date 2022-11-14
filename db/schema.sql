21ccc78148ee1c7387166247a9f7b767ea66e1c4
Vaughn Aubrey <vaubrey7@gmail.com>
2022-11-14 14:26:55 -0700

    updated starter code- still testing

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS taco_ecommerce_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE taco_ecommerce_db;
 
